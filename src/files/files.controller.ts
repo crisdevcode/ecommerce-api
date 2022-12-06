524738d3f969f4610a20f2a3ed992d85747dc299
Cristian Unigarro <ungcrisfit@gmail.com>
2022-12-06 11:43:57 -0500

    add: image secure url

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post, UploadedFile, UseInterceptors, BadRequestException, Get, Param, Res } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Response } from 'express';
 import { diskStorage } from 'multer';
@@ -7,7 +8,10 @@ import { fileNamer, fileFilter } from './helpers';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService
+  ) {}
 
   @Get('product/:imageName')
   findProductFile(
@@ -32,14 +36,14 @@ export class FilesController {
   uploadProductFile(
     @UploadedFile() file: Express.Multer.File,
   ){
-    console.log({ fileInController: file })
+    // console.log({ fileInController: file })
     if(!file) {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
-    return {
-      fileName: file.originalname
-    };
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
+
+    return { secureUrl };
   }
   
 }
