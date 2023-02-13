0ca7c2b4834dac155fbe9d1fb54f98a42fcf029c
Cristian Unigarro <ungcrisfit@gmail.com>
2023-02-13 10:59:27 -0500

    add: private route - general

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body} from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Get} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import {  } from './dto/login-user.dto';
@@ -15,5 +16,15 @@ export class AuthController {
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+  @Get('private')
+  @UseGuards( AuthGuard() )
+  testingPrivateRoute() {
+    return {
+      ok: true,
+      message: 'Hello',
+      user: { name: 'YourClownBaby' }
+    }
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -36,7 +36,7 @@ export class AuthService {
       // Return JWT to access.
       return { 
         ...user,
-        token: this.getJwtToken({ email: user.email })
+        token: this.getJwtToken({ id: user.id })
       };
       
     } catch (error) {
@@ -50,7 +50,7 @@ export class AuthService {
     
     const user = await this.userRepository.findOne({
       where: { email },
-      select: { email: true, password: true }
+      select: { email: true, password: true, id: true }
     });
     
     if (!user)
@@ -61,7 +61,7 @@ export class AuthService {
     
     return { 
       ...user,
-      token: this.getJwtToken({ email: user.email })
+      token: this.getJwtToken({ id: user.id })
     };
   }
 
